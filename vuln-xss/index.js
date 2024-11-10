16ca69101b061c50eb9a045545fb35bb4dcb09e4
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-11-10 19:52:31 +0700

    add: register

--- vuln-xss/index.js
+++ vuln-xss/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import express from "express";
 import * as path from 'path'
 import ejsmate from 'ejs-mate'
+import bcrypt from 'bcrypt'
 
 config()
 
@@ -11,11 +12,30 @@ mongoose.connect(process.env.MONGO)
 .catch(err => console.log(err))
 
 
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+
 const app = express()
 
 app.set('views', path.join(import.meta.dirname, 'views'))
 app.set('view engine', 'ejs')
 app.engine('ejs', ejsmate)
+
+app.use(express.urlencoded({extended: true}))
 
 
 app.get('/', (req, res) => {
@@ -25,6 +45,16 @@ app.get('/', (req, res) => {
 app.get('/register', (req, res) => {
     res.render('register')
 })
+
+app.post('/register', async(req, res) => {
+    const {username, password} = req.body
+    const newPasswd = await bcrypt.hash(password, 10)
+    const user = new User({username, password: newPasswd})
+    await user.save()
+    res.status(201).redirect('/')
+})
+
+
 app.get('/profile', (req, res) => {
     res.render('profile')
 })
