a90791d4839532003c5742a18dc420f5742e99ae
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-11-12 15:21:04 +0700

    add: gatau lah project baru aja biar aman

---
+++ react-auth/client/react-auth/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+function Nav(){
+
+    return (
+        <nav className="w-screen h-12 bg-sky-600 text-slate-200 p-2 flex gap-4 items-center justify-start">
+            <a href="#">Home</a>
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+        </nav>
+    )
+}
+
+
+export default Nav
