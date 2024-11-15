9bf991624e73cbe195b0b1c7934d758717227cde
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-11-15 17:47:21 +0700

    add: login

--- react-auth/client/src/views/Login.jsx
+++ react-auth/client/src/views/Login.jsx
@@ -1,18 +1,60 @@
+import { useState } from "react"
+import AxiosInstance from "../utils/AxiosInstance"
+
 /* eslint-disable react/prop-types */
 function Login({msg}){
+
+
+    const [data, setData] = useState({
+        username: "",
+        password: ""
+    })
+
+
+    const setUsername = (e) => {
+        setData({
+            ...data,
+            username: e.target.value
+        })
+    }
+    const setPassword = (e) => {
+        setData({
+            ...data,
+            password: e.target.value
+        })
+    }
+
+    const submitForm = async (e) => {
+        e.preventDefault()
+
+        
+        try {
+            const res = await AxiosInstance.post('/login', data)
+            console.log(res.data)
+            
+        } catch (error) {
+            console.log(error)
+            console.log('Redirect aja sana')
+        }
+        
+    }
+    
+    
+    
+    
     return(
         <>
             <h1 className="text-3xl">Login Page</h1>
             {msg ? (
                 <p className="text-green-400">{msg}</p>
             ) : ''}
-            <form className="w-1/2 h-72 flex flex-col items-center justify-center gap-4 border-2 border-blue-500 p-8 m-8 rounded-sm">
+            <form onSubmit={submitForm} className="w-1/2 h-72 flex flex-col items-center justify-center gap-4 border-2 border-blue-500 p-8 m-8 rounded-sm">
       
                     <label htmlFor="username">Username</label>
-                    <input className="focus:outline-sky-500 p-1 text-center text-slate-900" type="text" id="username" />
+                    <input onChange={setUsername} className="focus:outline-sky-500 p-1 text-center text-slate-900" type="text" id="username" />
       
                     <label htmlFor="password">Password</label>
-                    <input className="focus:outline-sky-500 p-1 text-center text-slate-900" type="text" id="password" />
+                    <input onChange={setPassword} className="focus:outline-sky-500 p-1 text-center text-slate-900" type="text" id="password" />
                     <div>
                         <input className="mr-4" type="checkbox" id="cek" />
                         <label htmlFor="cek">show password</label>
--- react-auth/server/index.js
+++ react-auth/server/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import { config } from "dotenv";
 import bcrypt from 'bcrypt'
 import cors from 'cors'
+import cookieParser from "cookie-parser";
+import jwt from 'jsonwebtoken'
 config()
 
 mongoose.connect(process.env.MONGO)
@@ -31,8 +33,31 @@ const PORT = process.env.PORT || 5000
 // middleware
 app.use(express.json()) // biar ngirimnya pake json bukan urlencoded form
 app.use(cors({
-    origin: '*'
+    origin: 'http://localhost:5173',
+    credentials: true
 }))
+app.use(cookieParser())
+
+
+const verifyToken = (req, res, next) => {
+    const {kuki} = req.cookies
+    if(!kuki) return res.status(403).json({
+        message: 'Not authorized',
+        status: 403,
+        redirect: '/login'
+    })
+
+    jwt.verify(kuki, process.env.JWT, (err, decode) => {
+        if(err) return res.status(403).json({
+            message: 'Not authorized',
+            status: 403,
+            redirect: '/login'
+        })
+        req.user = decode
+        next()
+    })
+
+}
 
 
 
@@ -50,6 +75,37 @@ app.post('/api/register', async (req, res) => {
         redirect: '/login',
         status: 201
     })
+})
+
+app.post('/api/login', async(req, res) => {
+
+    const {username, password} = req.body
+    
+    const user = await User.findOne({username})
+
+    
+    if(!user || !await bcrypt.compare(password, user.password)) return res.status(404).json({
+        message: 'Not found',
+        status: 404,
+        redirect: '/login'
+    })
+
+
+    const token = jwt.sign({username}, process.env.JWT, {
+        expiresIn: '1h',
+    })
+
+    res.cookie('kuki', token, {
+        httpOnly: true
+    })
+
+
+    res.status(200).json({
+        message: 'Login success',
+        status: 200,
+        redirect: '/dashboard'
+    })
+    
 })
 
 
