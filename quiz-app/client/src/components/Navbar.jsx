b6e4c29be104d9c8a89c2f6c77ded16846cb7eb3
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-11-25 14:50:54 +0700

    add: styling

--- quiz-app/client/src/components/Navbar.jsx
+++ quiz-app/client/src/components/Navbar.jsx
@@ -1,20 +1,46 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [navAct, setNavAct] = useState(false);
+
   return (
-    <nav className="w-full flex gap-8 p-6 font-bold bg-slate-300/30 text-slate-300 backdrop-blur-md fixed top-0 border-b-2 border-b-indigo-500 ">
-      <Link className="hover:text-indigo-500" to="/">
-        Home
-      </Link>
-      <Link className="hover:text-indigo-500" to="/quiz">
-        Quiz
-      </Link>
-      <Link className="hover:text-indigo-500" to="/about">
-        About
-      </Link>
-      <Link className="hover:text-indigo-500" to="/login">
-        Login
-      </Link>
-    </nav>
+    <>
+      <nav className="w-full flex gap-8 p-6 font-bold bg-slate-300/30 text-slate-300 backdrop-blur-md fixed top-0 border-b-2 border-b-indigo-500 justify-between">
+        <p>Learn App</p>
+
+        <div className="hidden md:flex w-1/3 gap-4">
+          <Link className="hover:text-indigo-500" to="/">
+            Home
+          </Link>
+          <Link className="hover:text-indigo-500" to="/quiz">
+            Quiz
+          </Link>
+          <Link className="hover:text-indigo-500" to="/about">
+            About
+          </Link>
+          <Link className="hover:text-indigo-500" to="/login">
+            Login
+          </Link>
+        </div>
+
+        <button
+          onClick={() => setNavAct(!navAct)}
+          className={
+            navAct
+              ? "flex bg-indigo-500 md:hidden w-16 h-16 rounded-md border-4 border-indigo-500 p-4"
+              : "flex md:hidden w-16 h-16 rounded-md border-4 border-indigo-500 p-4"
+          }
+        ></button>
+      </nav>
+
+      {navAct && (
+        <nav className="w-full relative  p-6 font-bold bg-slate-300/30 text-slate-300 backdrop-blur-md border-b-2  top-0 border-b-indigo-500">
+          <Link className="hover:text-indigo-500" to="/">
+            Home
+          </Link>
+        </nav>
+      )}
+    </>
   );
 }
--- quiz-app/client/src/views/Quiz.jsx
+++ quiz-app/client/src/views/Quiz.jsx
@@ -69,7 +69,7 @@ export default function Quiz() {
 
       <form
         onSubmit={addQuestionHandle}
-        className="flex flex-col gap-8 items-center"
+        className="w-full flex flex-col gap-8 items-center"
       >
         <div className="flex gap-8 justify-center items-center m-8 p-8">
           <label htmlFor="quiz">Add ur question in here</label>
@@ -130,6 +130,17 @@ export default function Quiz() {
               required
             />
           </section>
+
+          <section className="flex gap-8">
+            <p>Select your correct answer</p>
+            <select className="w-20 ml-4 text-center rounded-md bg-slate-900 border-2 border-indigo-500 hover:border-indigo-400 hover:bg-indigo-900">
+              <option></option>
+              <option value="a">A</option>
+              <option value="b">B</option>
+              <option value="c">C</option>
+              <option value="d">D</option>
+            </select>
+          </section>
           <button className="w-1/2 font-semibold mt-8 px-8 py-2 rounded-md border-2 border-indigo-500 mx-auto hover:bg-indigo-500 hover:text-slate-900 duration-100 active:animate-ping active:border-slate-900 active:text-slate-100">
             Add
           </button>
@@ -145,8 +156,11 @@ export default function Quiz() {
               key={index}
               className="flex min-w-32 mb-4 border-b-2 p-8 border-b-indigo-500 flex-col gap-8 items-start"
             >
-              <h1 className="border-b-2 border-b-indigo-500">
-                {index + 1}. {quiz.question}
+              <h1 className="border-t-2 border-t-indigo-500">
+                <span className="mr-4 font-semibold text-indigo-500">
+                  {index + 1}.
+                </span>{" "}
+                {quiz.question}
               </h1>
               <div>
                 <input
