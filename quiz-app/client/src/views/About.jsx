46c9e4161222ff21729a5761099feb5b1a3fbe73
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-11-30 20:53:45 +0700

    add: fa-icon

--- quiz-app/client/src/views/About.jsx
+++ quiz-app/client/src/views/About.jsx
@@ -1,9 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
 export default function About() {
   return (
     <>
       <h1 className="text-4xl p-8 font-bold bg-gradient-to-r from-indigo-500 to-slate-700 bg-clip-text text-transparent">
         About Page
       </h1>
+      <FontAwesomeIcon icon={faGithub} className="text-9xl" />
     </>
   );
 }
