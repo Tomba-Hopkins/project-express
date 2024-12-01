2c6112075416d4d0f2504e22307e61d0ba8e35b9
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-12-01 17:10:18 +0700

    add: styling

--- quiz-app/client/src/views/About.jsx
+++ quiz-app/client/src/views/About.jsx
@@ -24,17 +24,76 @@ export default function About() {
           Tech Stack
         </h1>
         <div className="w-full my-10 p-4 grid grid-cols-2 place-items-center gap-4">
-          <div className="p-4 gap-8 flex flex-col items-center w-1/2 border-2 border-indigo-500 rounded-sm">
-            <h1 className="bg-gradient-to-r from-slate-500 font-semibold to-slate-300 bg-clip-text text-transparent">
+          <div className="p-8 gap-8 flex flex-col items-center w-1/2 rounded-md">
+            <h1 className="text-2xl bg-gradient-to-r from-slate-500 font-semibold to-slate-300 bg-clip-text text-transparent">
               Frontend
             </h1>
-            <div className="w-full border-2 border-indigo-500 flex flex-wrap"></div>
+
+            <div className="w-full flex flex-wrap gap-8 justify-between items-center">
+              <img
+                className="w-1/4 h-1/4 grayscale hover:grayscale-0 duration-150"
+                src="/assets/react.svg"
+                alt="react"
+              />
+              <img
+                className="w-1/4 h-1/4 grayscale hover:grayscale-0 duration-150"
+                src="/assets/reactrouter.svg"
+                alt="react-router"
+              />
+              <img
+                className="w-1/4 h-1/4 grayscale hover:grayscale-0 duration-150"
+                src="/assets/vite.svg"
+                alt="vite"
+              />
+              <img
+                className="w-1/4 h-1/4 grayscale hover:grayscale-0 duration-150"
+                src="/assets/tailwindcss.svg"
+                alt="tailwind"
+              />
+              <img
+                className="w-1/4 h-1/4 grayscale hover:grayscale-0 duration-150"
+                src="/assets/fa.svg"
+                alt="font-awesome"
+              />
+            </div>
           </div>
-          <div className="p-4 gap-8 flex flex-col items-center w-1/2 border-2 border-indigo-500 rounded-sm">
-            <h1 className="bg-gradient-to-r from-slate-500 font-semibold to-slate-300 bg-clip-text text-transparent">
+          <div className="p-8 gap-8 flex flex-col items-center w-1/2 rounded-md">
+            <h1 className="text-2xl bg-gradient-to-r from-slate-500 font-semibold to-slate-300 bg-clip-text text-transparent">
               Backend
             </h1>
-            <div className="w-full border-2 border-indigo-500 flex flex-wrap"></div>
+
+            <div className="w-full flex flex-wrap gap-8 justify-between items-center">
+              <img
+                className="w-1/4 h-1/4 bg-white p-2"
+                src="/assets/express.svg"
+                alt="express"
+              />
+              <img
+                className="w-1/4 h-1/4 grayscale hover:grayscale-0 duration-150"
+                src="/assets/mongodb.svg"
+                alt="mongodb"
+              />
+              <img
+                className="w-1/4 h-1/4"
+                src="/assets/mongoose.svg"
+                alt="mongoose"
+              />
+              <img
+                className="w-1/4 h-1/4 grayscale hover:grayscale-0 duration-150"
+                src="/assets/node.svg"
+                alt="node"
+              />
+              <img
+                className="w-1/4 h-1/4 grayscale hover:grayscale-0 duration-150"
+                src="/assets/dotenv.svg"
+                alt="dotenv"
+              />
+              <img
+                className="w-1/4 h-1/4 grayscale hover:grayscale-0 duration-150"
+                src="/assets/docker.svg"
+                alt="docker"
+              />
+            </div>
           </div>
         </div>
       </section>
