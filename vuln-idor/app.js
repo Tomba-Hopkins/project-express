4ece04b4cde5c70cf1499a61fbc2501ab29c5a3f
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-12-27 19:58:47 +0700

    add: done

--- vuln-idor/app.js
+++ vuln-idor/app.js
@@ -4,47 +4,42 @@ import * as path from "path";
 
 const app = express();
 
+let mc = 0;
+
 const users = [
   {
     id: 1,
     name: "Mr. Nasi Goreng",
     identity_number: "1012005",
-    img: "",
+    img: "/img/nasgor.jpg",
     city: "Gotham City",
   },
   {
     id: 2,
     name: "Mr. Mie Goreng",
     identity_number: "1012006",
-    img: "",
+    img: "/img/mie.jpg",
     city: "Pizza City",
   },
   {
     id: 3,
     name: "Mr. Tahu Tek",
     identity_number: "1012006",
-    img: "",
+    img: "/img/tahu.jpg",
     city: "Mineral Town",
   },
   {
     id: 4,
     name: "Mrs. Strawberry",
     identity_number: "1012007",
-    img: "",
+    img: "/img/straw.jpg",
     city: "Candy City",
   },
   {
     id: 5,
-    name: "Sir Batagor",
-    identity_number: "1012008",
-    img: "",
-    city: "Unknown City",
-  },
-  {
-    id: 6,
     name: "FLAG{D0R_K4G3T_G4}",
     identity_number: "1012009",
-    img: "",
+    img: "/img/flag.jpg",
     city: "Flag City",
   },
 ];
@@ -52,6 +47,30 @@ app.set("view engine", "ejs");
 app.set("views", path.join(import.meta.dirname, "views"));
 app.use(express.static(path.join(import.meta.dirname, "public")));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  const id = users[mc].id;
+  const username = users[mc].name;
+  const image = users[mc].img;
+
+  res.render("home", {
+    id,
+    username,
+    image,
+  });
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  mc = id - 1;
+
+  const { name, img, city, identity_number } = users[mc];
+
+  res.render("profile", {
+    name,
+    img,
+    identity_number,
+    city,
+  });
+});
 
 app.listen(5000, () => console.log("Server Running on http://localhost:5000"));
