15b8317b32272b1f43c8abf28c58a642b4746282
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-12-28 19:19:28 +0700

    add: more page

--- vuln-bfla/app.js
+++ vuln-bfla/app.js
@@ -5,9 +5,16 @@ import * as path from "path";
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(path.join(import.meta.dirname, "public"));
+app.use(express.static(path.join(import.meta.dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(import.meta.dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
 
 app.listen(5000, () => console.log("server started on http://localhost:5000"));
